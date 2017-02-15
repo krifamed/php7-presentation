6ee02903e24f6b86f0d2b05a0866c5dfae2ab14d
crazy-max <webmaster@crazyws.fr>
2017-02-15 14:27:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<p align="center">
+  <a href="https://crazy-max.github.io/php7-presentation" target="_blank"><img src="https://crazy-max.github.io/php7-presentation/img/logo.png"></a>
+</p>
+<p align="center">Présentation PHP7</p>
+
+<p align="center">
+  <a href="https://travis-ci.org/crazy-max/php7-presentation"><img src="https://img.shields.io/travis/crazy-max/php7-presentation/master.svg?style=flat-square" alt="Build Status"></a>
+  <a href="https://www.codacy.com/app/crazy-max/php7-presentation"><img src="https://img.shields.io/codacy/grade/1c78d2c0aa814fd685b60868192d8d81.svg?style=flat-square" alt="Code Quality"></a>
+  <a href="https://gemnasium.com/github.com/crazy-max/php7-presentation"><img src="https://img.shields.io/gemnasium/crazy-max/php7-presentation.svg?style=flat-square" alt="Gemnasium"></a>
+  <a href="https://flattr.com/submit/auto?user_id=crazymax&url=https://crazy-max.github.io/php7-presentation"><img src="https://img.shields.io/badge/flattr-this-green.svg?style=flat-square" alt="Flattr this!"></a>
+  <a href="https://crazy-max.github.io/php7-presentation"><img src="https://app.statuscake.com/button/index.php?Track=mZ96VTLi6T&Days=1&Design=3" alt="StatusCake Uptime"></a>
+</p>
+
+## About
+
+An overview over the improvments coming with PHP 7 (in French) using RevealJS HTML slide presentation and Jekyll.
+
+## Prerequisites
+
+To work on the website, you'll need the following things installed on your machine :
+
+1. [Ruby](https://www.ruby-lang.org/en/documentation/installation/) - choose an available tool to install Ruby on your system.
+2. [Bundler](https://bundler.io/) - `$ gem install bundler`
+3. [Jekyll](http://jekyllrb.com/) - `$ gem install jekyll`
+4. [Node.js](http://nodejs.org) - use the installer.
+5. [gulp.js](https://github.com/gulpjs/gulp) - `$ npm install -g gulp`
+6. [Bower](https://github.com/bower/bower) - `$ npm install -g bower`
+
+## Installation
+
+1. Clone this repo, or download it into a directory of your choice.
+2. Inside the directory, run `bundle install`, `npm install` and `bower install`.
+
+## Usage
+
+This will give you file watching, browser synchronisation, auto-rebuild, CSS/JS and vendor injecting :
+
+```shell
+$ gulp serve
+```
+
+Then open your browser at http://localhost:3000<br />
+As this is just a Jekyll project, you can use any of the commands listed in their [docs](http://jekyllrb.com/docs/usage/).
+
+## Credits
+
+* [reveal.js](https://github.com/hakimel/reveal.js)
+* [PHP 7 logo](http://www.cowburn.info/2015/06/18/php7-logo/)
+
+## License
+
+LGPL-3.0. See `LICENSE` for more details.
---
+++ gulp-tasks/assets-vendor.js
@@ -0,0 +1,105 @@
+var gulp = require("gulp"),
+  readConfig = require("read-config"),
+  config = readConfig("_config.yml"),
+  del = require("del"),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  mainBowerFiles = require("main-bower-files"),
+  inject = require("gulp-inject"),
+  filter = require("gulp-filter"),
+  order = require("gulp-order"),
+  cleanCSS = require("gulp-clean-css"),
+  Bust = require("gulp-bust"),
+  merge = require("merge-stream"),
+  gutil = require("gulp-util");
+
+var bust = new Bust();
+var assetsVendorToInject = [];
+
+function string_src(filename, string) {
+  var src = require("stream").Readable({ objectMode: true });
+  src._read = function () {
+    this.push(new gutil.File({
+      cwd: "",
+      base: "",
+      path: filename,
+      contents: new Buffer(string)
+    }));
+    this.push(null)
+  };
+  return src;
+}
+
+gulp.task("assets-vendor-build", [
+  "assets-vendor-init",
+  "assets-vendor-clean",
+  "assets-vendor-styles",
+  "assets-vendor-scripts",
+  "assets-vendor-gfonts",
+  "assets-vendor-others",
+  "assets-vendor-inject"
+]);
+
+gulp.task("assets-vendor-init", function() {
+  assetsVendorToInject = [];
+  return null;
+});
+
+gulp.task("assets-vendor-clean", function(done) {
+  del([
+    config.destination + "/" + config.paths.assets + "/**/*",
+    "!" + config.destination + "/" + config.paths.assets + "/*.js",
+    "!" + config.destination + "/" + config.paths.assets + "/*.css"
+  ]);
+  done();
+});
+
+gulp.task("assets-vendor-scripts", function() {
+  assetsVendorToInject = merge(assetsVendorToInject, gulp.src(mainBowerFiles(), { base: config.paths.bower })
+    .pipe(filter(["**/*.js", "**/*.map", "!**/*.css.map"]))
+    .pipe(order(["**/jquery.js", "**/reveal.js", "**/*.js"]))
+    .pipe(gutil.env.env === "production" ? uglify() : gutil.noop())
+    .pipe(gutil.env.env === "production" ? bust.resources() : gutil.noop())
+    .pipe(gulp.dest(config.destination + "/" + config.paths.assets)));
+});
+
+gulp.task("assets-vendor-styles", function() {
+  assetsVendorToInject = merge(assetsVendorToInject, gulp.src(mainBowerFiles(), { base: config.paths.bower })
+    .pipe(filter(["**/*.css", "**/*.css.map"]))
+    .pipe(order(["**/reveal.css", "**/theme/black.css", "**/*.css"]))
+    .pipe(gutil.env.env === "production" ? cleanCSS() : gutil.noop())
+    .pipe(gutil.env.env === "production" ? bust.resources() : gutil.noop())
+    .pipe(gulp.dest(config.destination + "/" + config.paths.assets)));
+});
+
+gulp.task("assets-vendor-gfonts", function() {
+  assetsVendorToInject = merge(assetsVendorToInject, gulp.src(mainBowerFiles(), { base: config.paths.bower })
+    .pipe(filter(["**/index"]))
+    .pipe(gutil.env.env === "production" ? cleanCSS() : gutil.noop())
+    .pipe(rename({ suffix: ".css" }))
+    .pipe(gutil.env.env === "production" ? bust.resources() : gutil.noop())
+    .pipe(gulp.dest(config.destination + "/" + config.paths.assets)));
+});
+
+gulp.task("assets-vendor-others", function() {
+  return gulp.src(mainBowerFiles(), { base: config.paths.bower })
+    .pipe(filter(["**/*.eot", "**/*.woff", "**/*.svg", "**/*.ttf", "**/*.woff2",
+      "**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif",
+      "**/*.html"]))
+    .pipe(gulp.dest(config.destination + "/" + config.paths.assets));
+});
+
+gulp.task("assets-vendor-inject", function() {
+  return string_src(config.source + "/_data/" + config.assets.vendor, "{\n  \"js\": [\n  ],\n  \"css\": [\n  ],\n  \"html\": [\n  ]\n}")
+    .pipe(inject(assetsVendorToInject, {
+      empty: true,
+      ignorePath: config.destination,
+      starttag: "\"{{ext}}\": [",
+      endtag: "]",
+      transform: function (filepath, file, i, length) {
+        return "  \"" + filepath + "\"" + (i + 1 < length ? "," : "");
+      }
+    }))
+    .pipe(gulp.dest("."));
+});
